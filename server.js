d7e17bf4f57930395b26b6f3a1d681e38a275311
Samuel Gay <sstikel@gmail.com>
2019-03-09 00:44:55 -0700

    better look

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
  * remainder of this semester (Jan - Apr 2019)
  */
 
-//const http = require("http");
+const http = require("http");
 //const fs = require('fs');
 const express = require("express");
 const app = express();
@@ -16,8 +16,9 @@ const port = process.env.PORT || 5000; //checks for heroku port OR use 5000
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-//app.use(express.static("public")); //let all files in 'public' be used anyways
+app.use(express.static("public")); //let all files in 'public' be used anyways
 app.use(express.static("postage"));
+
 app.post("/postageResult", postageResult);
 
 app.listen(port, function () {
