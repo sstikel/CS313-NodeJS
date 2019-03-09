c6805b2f543b4059c2b7c4f0b219df8690be1644
Samuel Gay <sstikel@gmail.com>
2019-03-09 00:20:58 -0700

    icon, other updates

--- server.js
+++ server.js
@@ -6,8 +6,8 @@
  * remainder of this semester (Jan - Apr 2019)
  */
 
-const http = require("http");
-const fs = require('fs');
+//const http = require("http");
+//const fs = require('fs');
 const express = require("express");
 const app = express();
 
@@ -16,10 +16,27 @@ const port = process.env.PORT || 5000; //checks for heroku port OR use 5000
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.use(express.static("public")); //let all files in 'public' be used anyways
+//app.use(express.static("public")); //let all files in 'public' be used anyways
 app.use(express.static("postage"));
-app.post("/postageResult");
+app.post("/postageResult", postageResult);
 
 app.listen(port, function () {
   console.log("Listening on port : " + port);
 });
+
+function postageResult(request, response) {
+  console.log("Called postageResult...");
+
+  const type = request.query.type;
+  console.log("Type: " + type);
+
+  const weight = request.query.weight;
+  console.log("Weight: " + weight);
+
+  const params = {
+    type: type,
+    weight: weight
+  };
+
+  response.render("postageResult", params);
+}
