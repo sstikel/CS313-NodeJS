4efa3ab0521dc28ccb1c5bb48207d4dce36af9d2
Samuel Gay <sstikel@gmail.com>
2019-03-19 11:15:25 -0600

    db update

--- library/model/dbAccess.js
+++ library/model/dbAccess.js
@@ -4,3 +4,13 @@
  * Date: 3/16/19
  * Purpose: Access library db at heroku
  ***************************************/
+
+var express = require('express');
+var router = express.Router();
+
+//pg config
+var pg = require('pg');
+var conString = 'postgres://@localhost/pg_demo_db';
+
+
+module.exports = router;
