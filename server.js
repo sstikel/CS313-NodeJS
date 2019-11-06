e2f96eea6a7e36459bb02e39b6e9824e6f257ea5
Samuel Gay <sstikel@gmail.com>
2019-11-06 16:04:38 -0700

    continued work on login process; little cleanup elsewhere

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 const{Pool} = require('pg');
 //const pgDB = require("pg");
-var conString = 'postgres://@localhost/pg_demo_db';
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 
@@ -22,18 +21,27 @@ app.use(bodyParser.json());
 
 ////db connection ////
 ////session////
+//https://codeforgeek.com/manage-session-using-node-js-express-4/
 app.use(require('morgan')('dev'));
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 app.use(session({
   name: 'server-session-cookie-id',
   secret: 'my express secret',
   saveUninitialized: true,
   resave: true,
   store: new FileStore()}));
+app.use(express.urlencoded());
+app.use(express.json());
 require('dotenv').config();
 
+//Heroku OAuth
+//https://github.com/heroku/node-heroku-client
+const Heroku = require('heroku-client');
+const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN });
+
 //Heroku pg db access - 10/22/19
+var conString = 'postgres://@localhost/pg_demo_db';
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -47,9 +55,6 @@ var db;
 //end
 
 const port = process.env.PORT || 5000; //checks for heroku port OR use 5000
-
-app.use(express.urlencoded());
-app.use(express.json());
 
 
 app.get("/home", function(req, res){
@@ -167,16 +172,59 @@ app.delete("/api/item/:id", async (req, res) => {
 
 //////////////////// USERS /////////////////////////////////////////
 //login
+  //authenticate
 app.get("/api/user/login", async (req, res) => {
-  //TODO - retrieve login info from req
-  //TODO - redirect to Heroku OAuth API
-    //GET https://id.heroku.com/oauth/authorize?client_id={client-id}&response_type=code&scope={scopes}&state={anti-forgery-token}
-  //TODO - return result (including cookie)
+  try{
+    //TODO - retrieve login info from req
+    let currentSession = req.session;
+    //TODO - sanitize clientID
+    currentSession.clientID = req.body.clientID;
+    currentSession.scope = 'read';
+    currentSession.state = '???';
+    //GET https://id.heroku.com/oauth/authorize?client_id={client-id}&response_type=code&scope=read&state={anti-forgery-token}
+    let herokuURL = "https://id.heroku.com/oauth/authorize?client_id=" + clientID + "&response_type=code&scope=" + scope + "&state=" + state;
 
+    //TODO - redirect to Heroku OAuth API
+    res.redirect(herokuURL);
+    
+    //TODO - store cookie + clientID to db???
+    //TODO - return result (including cookie)
+    res.end("User Authenticated.");
+  }
+  catch (err){
+
+  }
+});
+
+  //already authenticated
+app.post("/api/user/login", async (req, res) => {
+  try{
+    //TODO - retrieve login info from req
+    let currentSession = req.session;
+    //TODO - compare cookie
+    //TODO - return result
+  }
+  catch (err){
+
+  }
 });
 //end login
 //logout
+app.get("/api/user/logout", async (req, res) => {
+  try{
+    //TODO - deauthorize cookie/token/etc.
+    req.session.destroy((err) => {
+      if(err){
+        console.log("error: " + err);
+        return "An error has occured in Logout.";
+      }
+    });
+    //TODO - return result (logout confirmation)
+  }
+  catch(err){
 
+  }
+});
 //end logout
 //create user
 
