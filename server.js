ae8a0992789efbcdbf719902f1a919fa36f942d0
Samuel Gay <sstikel@gmail.com>
2019-11-11 19:27:09 -0700

    implemented login, logout, new user api endpoints including bcrypt for password comparison and session tracking.

--- server.js
+++ server.js
@@ -10,81 +10,56 @@
 const http = require("http");
 const express = require("express");
 const router = express.Router();
+//var conString = 'postgres://@localhost/pg_demo_db';
 const bodyParser = require("body-parser");
 const{Pool} = require('pg');
+//Pool -- https://node-postgres.com/api/pool
 //const pgDB = require("pg");
 const bcrypt = require('bcrypt');
+// bcrypt -- https://www.npmjs.com/package/bcrypt
 require('dotenv').config();
 
 var app = express();
 app.use(bodyParser.json());
+const dbConnection = process.env.DATABASE_URL;
+const pool = new Pool({connectionString: dbConnection});
 
 ////db connection ////
 ////session////
 //https://codeforgeek.com/manage-session-using-node-js-express-4/
 app.use(require('morgan')('dev'));
 const session = require('express-session');
+//express-session -- https://www.npmjs.com/package/express-session
 const FileStore = require('session-file-store')(session);
 app.use(session({
   name: 'server-session-cookie-id',
   secret: 'my express secret',
-  saveUninitialized: true,
+  saveUninitialized: false,
   resave: true,
   store: new FileStore()}));
 app.use(express.urlencoded());
 app.use(express.json());
-require('dotenv').config();
 
-//Heroku OAuth
-//https://github.com/heroku/node-heroku-client
-const Heroku = require('heroku-client');
-const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN });
-
-//Heroku pg db access - 10/22/19
-var conString = 'postgres://@localhost/pg_demo_db';
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
-var LIBRARY_DATA = "lib.library";
-var db;
-
-//connect to db
 //end
 
 const port = process.env.PORT || 5000; //checks for heroku port OR use 5000
 
-
+//Homepage from CS313
 app.get("/home", function(req, res){
   res.redirect("home.html");
-})
+});
 
-//////////////////// LIBRARY ///////////////////////////////////////////
-// - 10/22/19
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM test_table'); //TODO - mod. needed
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error: " + err);
-  }
-})
-//https://devcenter.heroku.com/articles/getting-started-with-nodejs#provision-a-database
-//end
-
-////  LIBRARY API ////
+//////////////////////////////  LIBRARY API //////////////////////////////
 //GET//
+
 //return full library
 app.get("/api/item", async (req, res) => {
   try{
-    //TODO - verify login info
     //TODO - sanatize input
+    //TODO - verify login info
+      //TODO - if cookie: compare cookies
+      //TODO - else: redirect to login
+      
     //TODO - query db for items
     //TODO - request items' info from external api(s)
         //- title
@@ -95,8 +70,23 @@ app.get("/api/item", async (req, res) => {
   }
   catch(err){
     console.error(err);
+    console.log(err);
     res.send("Error: " + err); //TODO - edit response for presentation to user
   }
+
+  /*
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table'); //TODO - mod. needed
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error: " + err);
+  }
+  */
+
 });
 
 //return specific library item
@@ -164,70 +154,102 @@ app.delete("/api/item/:id", async (req, res) => {
 });
 //END DELETE//
 
-
-//https://devcenter.heroku.com/articles/mean-apps-restful-api
 ////  END LIBRARY API ////
-
-//////////////////// END LIBRARY ///////////////////////////////////////////
-
 //////////////////// USERS /////////////////////////////////////////
 //login
-  //authenticate
 app.get("/api/user/login", async (req, res) => {
   try{
     //TODO - retrieve login info from req
-    let currentSession = req.session;
-    //TODO - sanitize clientID
-    currentSession.clientID = req.body.clientID;
-    currentSession.scope = 'read';
-    currentSession.state = '???';
-    //GET https://id.heroku.com/oauth/authorize?client_id={client-id}&response_type=code&scope=read&state={anti-forgery-token}
-    let herokuURL = "https://id.heroku.com/oauth/authorize?client_id=" + clientID + "&response_type=code&scope=" + scope + "&state=" + state;
-
-    //TODO - redirect to Heroku OAuth API
-    res.redirect(herokuURL);
-    
-    //TODO - store cookie + clientID to db???
-    //TODO - return result (including cookie)
-    res.end("User Authenticated.");
+    let sess = req.session;
+    //TODO - sanitize user input
+    let username = req.username;
+    //query db 
+    let sql = 'SELECT h_password FROM lib.user WHERE username=' + username;
+    let h_password;
+    pool.query(sql, username, (err, result) => {
+      if(err){
+        console.log("Error with DB(login/username)");
+        console.log(err);
+      }    
+      else
+        h_password = result.rows[0].h_password;
+    });
+    //compare hashed passwords
+    let match = await bcrypt.compare(req.password, h_password);
+    if(match){
+      //set cookie
+      sess.cookie.path('/api/item');
+       //return result
+      res.send("User Authenticated.");
+    }
+    else
+      res.send("Login info incorrect.")   
   }
   catch (err){
-
-  }
-});
-
-  //already authenticated
-app.post("/api/user/login", async (req, res) => {
-  try{
-    //TODO - retrieve login info from req
-    let currentSession = req.session;
-    //TODO - compare cookie
-    //TODO - return result
-  }
-  catch (err){
-
+    console.log("Error with API login");
+    console.log(err);
   }
 });
 //end login
+
 //logout
 app.get("/api/user/logout", async (req, res) => {
   try{
-    //TODO - deauthorize cookie/token/etc.
-    req.session.destroy((err) => {
+    //deauthorize cookie/token/etc.
+    let sess = req.session;
+    sess.destroy((err) => {
       if(err){
-        console.log("error: " + err);
+        console.log("error with API logout: " + err);
         return "An error has occured in Logout.";
       }
     });
     //TODO - return result (logout confirmation)
+    res.send("Logout complete");
   }
   catch(err){
-
+    console.log("Error with API logout");
+    console.log(err);
   }
 });
 //end logout
 //create user
-
+app.put("/api/user/newuser", async (req, res) => {
+  try{
+    let inserted = false;
+    var hash = "";
+    //TODO - sanatize input
+    let username = req.username;
+    let password = req.password;
+    let name_first = req.name_first;
+    let name_last = req.name_last;
+    //query db -- store username, h_password, first and last names
+    bcrypt.hash(password, 10, (err, hash) => {
+      const sql = "INSERT INTO lib.user (username, h_password, name_first, name_last) VALUES ($1, $2, $3, $4)";
+      const params = [username, h_password, name_first, name_last];
+  
+      pool.query(sql, params, (err, result) => {
+        if (err){
+          console.log("Error inserting new user into DB.");
+          console.log(err);
+        }
+        else 
+          inserted = true;
+      });
+    }
+    //redirect to login  
+    if(inserted){  
+      res.writeHead(302, {
+        'Location': "/api/user/login"
+      });
+      //res.redirect("/api/user/login");
+    }
+  }
+  catch(err){
+    console.log("Error with new user on API");
+    console.log(err);
+    res.send("Error creating new user;")
+  }
+});
 //end create user
 //////////////////// END USERS /////////////////////////////////////////
 
@@ -238,3 +260,5 @@ app.listen(port, function () {
 ////////////////////// END LISTENER ////////////////////////////
 
 /////////////////////functions///////////////////////////
+
+
